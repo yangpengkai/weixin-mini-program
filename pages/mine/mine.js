7b32721e2f6a5e47dfb1f8ac959af7c6d04bc7f6
ypk <3196241438@qq.com>
2021-01-05 11:48:07 +0800

    微信小程序->事件绑定

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -2,10 +2,48 @@
 Page({
 
   /**
-   * Page initial data
+   * 页面初始化数据
    */
   data: {
+    msg: "hello mina-program",
+    list: [{
+        id: 0,
+        name: '张三',
+        age: 18
+      },
+      {
+        id: 1,
+        name: '李四',
+        age: 20
+      },
+      {
+        id: 2,
+        name: '王五',
+        age: 21
+      }
+    ],
+    isagent:1,
+    ishidden:false,
+    num:0
+  },
 
+  oninput(e){
+    this.setData({
+      num:e.detail.value
+    })
+  },
+//这里是添加和删除按钮的事件
+  handletap(e){
+    const operation = e.currentTarget.dataset.operation;
+    this.setData({
+      num:this.data.num + operation
+    })
+  },
+
+  handleishidden(){
+    this.setData({
+      ishidden:!ishidden,
+    })
   },
 
   /**
@@ -26,10 +64,7 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow: function() {
-    wx.showToast({
-      title: '弹点东西出来看看',
-      icon: 'none'
-    })
+
   },
 
   /**
